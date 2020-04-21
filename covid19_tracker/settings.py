dde1753abf8696da1459a1cb3049b2bfece8a4a4
Shivam Bansal <shivambansal157@gmail.com>
2020-04-22 02:04:09 +0530

    fix settings

--- covid19_tracker/settings.py
+++ covid19_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*v4!36)^3ebz&bi%*+conn00(o%7$$4k!&%x-h12zzhg))en1w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
